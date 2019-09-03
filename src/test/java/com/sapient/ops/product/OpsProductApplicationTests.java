6bdbd8ddbdbc9043c01a4b0da1f1229b97a558b8
Akash Agrawal <akashagrawal01010@gmail.com>
2019-09-03 10:56:19 +0530

    Update OpsProductApplicationTests.java

--- src/test/java/com/sapient/ops/product/OpsProductApplicationTests.java
+++ src/test/java/com/sapient/ops/product/OpsProductApplicationTests.java
@@ -1,16 +1,16 @@
-package com.sapient.ops.product;
+// package com.sapient.ops.product;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// // import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class OpsProductApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class OpsProductApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+// 	@Test
+// 	public void contextLoads() {
+// 	}
 
-}
+// }
